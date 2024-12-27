50dce9bbb9143d6017bf8d2c056011d482623720
tangledcircuit <tangledcircuit@gmail.com>
2024-12-26 21:28:03 -0600

    fix: adjust main.ts for Deno Deploy

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
-import { serve } from "std/http/server.ts";
 import { router } from "./api.ts";
 
-// Serve the API on port 8000
-serve(router, { port: 8000 });
+// Export the request handler for Deno Deploy
+export default router;
